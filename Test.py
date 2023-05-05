ced9be748d92e046f658c1caf4f150b995371f3c
AungZawHtet2002 <aungzawhtetko@gmail.com>
2023-05-05 14:05:12 +0630

    add test file

--- Test.py
+++ Test.py
@@ -3,12 +3,15 @@ from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
 import time
+import tensorflow as tf
 cap=cv2.VideoCapture(0)
 detector= HandDetector(maxHands=1)
 offset=30                    
 imgsize=256
-folder='Hand Detection/Go'   #Folder Path
-counter = 0                  #To count saved image
+
+model=tf.keras.models.load_model("model")   #load our model
+cap= cv2.VideoCapture(0)
+labels_dict={0 : "Go", 1 : "Turn Right", 2 : "Turn Left"}     #Creating label dictionary for our output class
 while True:
     success,img=cap.read()
     hands,img =  detector.findHands(img)
@@ -34,9 +37,11 @@ while True:
         cv2.imshow("Croped Image",whiteimg)
     cv2.imshow("Image",img)
     key=cv2.waitKey(1)
-    if key == ord("s"):
-        counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',whiteimg)
-        print(counter)
-    if cv2.waitKey(0) & 0xFF == ord ('q'):
-        break
+    prediction=model.predict(whiteimg)
+    output=labels_dict[int(prediction[0])]
+    print(output)
+    cv2.imshow("Video",img)
+    cv2.waitKey(0)
+
+cap.release()
+cv2.destroyAllWindows()
