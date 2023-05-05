8a38474c6aec59e56ee3d85c6d8c005429720181
AungZawHtet2002 <aungzawhtetko@gmail.com>
2023-05-05 20:06:06 +0630

    Ready to use

--- Test.py
+++ Test.py
@@ -2,7 +2,6 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
-import time
 import tensorflow as tf
 cap=cv2.VideoCapture(0)
 detector= HandDetector(maxHands=1)
@@ -11,7 +10,7 @@ imgsize=256
 
 model=tf.keras.models.load_model("model")   #load our model
 cap= cv2.VideoCapture(0)
-labels_dict={0 : "Go", 1 : "Turn Right", 2 : "Turn Left"}     #Creating label dictionary for our output class
+class_name=['Go','Left','Right']
 while True:
     success,img=cap.read()
     hands,img =  detector.findHands(img)
@@ -35,13 +34,13 @@ while True:
             h_gap= math.ceil((imgsize-new_h)/2)
             whiteimg[h_gap:new_h+h_gap,:]=resize_img
         cv2.imshow("Croped Image",whiteimg)
-    cv2.imshow("Image",img)
-    key=cv2.waitKey(1)
-    prediction=model.predict(whiteimg)
-    output=labels_dict[int(prediction[0])]
-    print(output)
+    image = np.expand_dims(whiteimg, axis=0)
+    predictions=model.predict(image)
+    predicted_class=class_name[np.argmax(predictions[0])]
+    print(predicted_class)
     cv2.imshow("Video",img)
-    cv2.waitKey(0)
+    if cv2.waitKey(0) & 0xFF == ord ('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
