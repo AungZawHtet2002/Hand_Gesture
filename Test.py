45663bc73dc46b155e959bc441dcf57f4ff647ac
AungZawHtet2002 <aungzawhtetko@gmail.com>
2023-05-08 11:11:33 +0630

    Print result when hand style changes

--- Test.py
+++ Test.py
@@ -2,15 +2,15 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
-import tensorflow as tf
-cap=cv2.VideoCapture(0)
+import tensorflow as tf 
+
 detector= HandDetector(maxHands=1)
 offset=30                    
 imgsize=256
-
-model=tf.keras.models.load_model("model")   #load our model
-cap= cv2.VideoCapture(0)
 class_name=['Go','Left','Right']
+model=tf.keras.models.load_model("hand_model1.h5")   #load our model
+cap= cv2.VideoCapture(0)
+previous= "G"
 while True:
     success,img=cap.read()
     hands,img =  detector.findHands(img)
@@ -37,7 +37,9 @@ while True:
     image = np.expand_dims(whiteimg, axis=0)
     predictions=model.predict(image)
     predicted_class=class_name[np.argmax(predictions[0])]
-    print(predicted_class)
+    if predicted_class!= previous:
+            previous=predicted_class
+            print(predicted_class)
     cv2.imshow("Video",img)
     if cv2.waitKey(0) & 0xFF == ord ('q'):
         break
