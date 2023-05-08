1049efcaae10b15d0b0134ef766c215534bba4a1
AungZawHtet2002 <aungzawhtetko@gmail.com>
2023-05-08 11:15:11 +0630

    Data Collector File

--- Hand_data_collector.py
+++ Hand_data_collector.py
@@ -3,22 +3,25 @@ from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
 import time
+
 cap=cv2.VideoCapture(0)
 detector= HandDetector(maxHands=1)
 offset=30                    
 imgsize=256
 folder='Hand Detection/Go'   #Folder Path
 counter = 0                  #To count saved image
+
 while True:
     success,img=cap.read()
     hands,img =  detector.findHands(img)
+
     if hands:
-        hand=hand[0]
+        hand=hands[0]
         x,y,w,h=hand['bbox']
         whiteimg=np.ones((imgsize,imgsize,3),np.uint8)
         croped_img=img[ y-offset:y+h+offset , x-offset:x+w+offset ]
-        
         ratio= h/w
+
         if ratio > 1 :
             constant=imgsize/h
             new_w= math.ceil(constant * w)
@@ -31,8 +34,11 @@ while True:
             resize_img=cv2.resize(croped_img,(imgsize,new_h))
             h_gap= math.ceil((imgsize-new_h)/2)
             whiteimg[h_gap:new_h+h_gap,:]=resize_img
+
         cv2.imshow("Croped Image",whiteimg)
+
     cv2.imshow("Image",img)
+
     key=cv2.waitKey(1)
     if key == ord("s"):
         counter += 1
@@ -40,3 +46,6 @@ while True:
         print(counter)
     if cv2.waitKey(0) & 0xFF == ord ('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
